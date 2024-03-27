8a74334151eb0ed335dcd39e122ec873ccabc02c
luccasirineu <luccas19100@outlook.com>
2024-03-27 15:44:25 -0300

    Criando classe test

--- src/main/java/com/luccasdev/springmongo/resources/UserResource.java
+++ src/main/java/com/luccasdev/springmongo/resources/UserResource.java
@@ -1,7 +1,6 @@
 package com.luccasdev.springmongo.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
