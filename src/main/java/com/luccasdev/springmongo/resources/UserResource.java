dd09d38caa335249255fb1cbf3c91199c5a00606
luccasirineu <luccas19100@outlook.com>
2024-03-28 16:43:54 -0300

    Criando o insert user

--- src/main/java/com/luccasdev/springmongo/resources/UserResource.java
+++ src/main/java/com/luccasdev/springmongo/resources/UserResource.java
@@ -1,6 +1,7 @@
 package com.luccasdev.springmongo.resources;
 
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -8,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.luccasdev.springmongo.domain.User;
 import com.luccasdev.springmongo.dto.UserDTO;
@@ -34,6 +38,15 @@ public class UserResource {
 	public ResponseEntity<UserDTO> findById(@PathVariable String id) { //Path variable serve para casar o id do argumento com o id da url		
 		User obj = service.findById(id); 
 		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDTO) { //RequestBody tem a msm funcao que o path variable mas é para o POST		
+		User obj = service.fromDTO(objDTO); // convertendo objDTO para obj
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri(); 
+
+		return ResponseEntity.created(uri).build();
 	}
 	
 
--- src/main/java/com/luccasdev/springmongo/services/UserService.java
+++ src/main/java/com/luccasdev/springmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.luccasdev.springmongo.domain.User;
+import com.luccasdev.springmongo.dto.UserDTO;
 import com.luccasdev.springmongo.repositories.UserRepository;
 import com.luccasdev.springmongo.services.exceptions.ObjectNotFoundException;
 
@@ -24,4 +25,12 @@ public class UserService {
 		Optional<User> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 		}
+	
+	public User insert(User obj) {
+		return repo.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
 }
