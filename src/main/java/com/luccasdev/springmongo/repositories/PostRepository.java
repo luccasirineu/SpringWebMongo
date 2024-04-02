5ee95b4b54338bff8f886d3744623faaf4a915c5
luccasirineu <luccas19100@outlook.com>
2024-04-02 17:39:34 -0300

    Consulta simples com query methods

--- src/main/java/com/luccasdev/springmongo/repositories/PostRepository.java
+++ src/main/java/com/luccasdev/springmongo/repositories/PostRepository.java
@@ -1,5 +1,7 @@
 package com.luccasdev.springmongo.repositories;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.luccasdev.springmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{ // string pois é o tipo do id
 
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/luccasdev/springmongo/resources/PostResource.java
+++ src/main/java/com/luccasdev/springmongo/resources/PostResource.java
@@ -3,14 +3,18 @@ package com.luccasdev.springmongo.resources;
 
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.luccasdev.springmongo.domain.Post;
+import com.luccasdev.springmongo.resources.util.URL;
 import com.luccasdev.springmongo.services.PostService;
 
 
@@ -27,6 +31,14 @@ public class PostResource {
 	public ResponseEntity<Post> findById(@PathVariable String id) { //Path variable serve para casar o id do argumento com o id da url		
 		Post obj = service.findById(id); 
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	
+	@GetMapping(value="/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue ="") String text) { 
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 	
 
